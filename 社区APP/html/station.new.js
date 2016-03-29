323be05b76f93d5cc8e894fd4a9e32e0e4e80674
lirlu <tan@oya.cc>
2016-03-29 10:29:51 +0800

    无用，只是放这看下，防万一要改

---
+++ 社区APP/html/station.new.js
@@ -0,0 +1,180 @@
+var _Data = {'lat':'', 'lng':'', 'key':app.store('key'), 'page':0, 'limit':1, 'category':''};
+
+mui.init();
+
+// 切换显示类型
+$('[data-page]').on('tap', function () {
+	$(this).removeClass('activet').addClass('activet').siblings().removeClass('activet');
+	var page = $(this).attr('data-page');
+	plus.webview.getWebviewById(page).show();
+});
+
+mui('.mui-scroll-wrapper').scroll({
+	indicators: true //是否显示滚动条
+});
+
+function next (cb) {
+	console.log('查询数据：' + JSON.stringify(_Data));
+	//plus.nativeUI.showWaiting('加载中...');
+	$.ajax({
+		'dataType' : 'json',
+		'type'     : 'post',
+		'url'      : app.url('mobile/index/water_seller'),
+		'data'     : mui.extend({}, _Data, {'page':_Data.page})
+	})
+	.fail(function (res) {
+		console.log('取得水站商家失败：' + JSON.stringify(res));
+		app.error('取得水站商家失败');
+		plus.nativeUI.closeWaiting();
+	})
+	.done(function (res) {
+		console.log('水站商家：' + JSON.stringify(res));
+		plus.nativeUI.closeWaiting();
+		
+		if (res.error && res.error.msg) { app.error(res.error.msg); return; }
+		if (false == res.status) {app.error(res.msg); return;};
+		if (res.msg) { plus.nativeUI.toast(res.msg); };
+		
+		_Data.page++;
+		cb && cb(res);
+	})
+	;
+}
+
+function funcPullupRefresh () {
+	next(function (res) {
+		var noMore = _Data.page * _Data.limit >= (res.total||1);
+		$('#pnl-shop').append(template('tpl-shop', res));
+		/*
+		$('#pnl-shop').append(template('tpl-shop', res));
+		$('#pnl-shop').append(template('tpl-shop', res));
+		$('#pnl-shop').append(template('tpl-shop', res));
+		$('#pnl-shop').append(template('tpl-shop', res));
+		$('#pnl-shop').append(template('tpl-shop', res));
+		$('#pnl-shop').append(template('tpl-shop', res));
+		$('#pnl-shop').append(template('tpl-shop', res));
+		$('#pnl-shop').append(template('tpl-shop', res));
+		$('#pnl-shop').append(template('tpl-shop', res));
+		$('#pnl-shop').append(template('tpl-shop', res));
+		$('#pnl-shop').append(template('tpl-shop', res));
+		$('#pnl-shop').append(template('tpl-shop', res));
+		$('#pnl-shop').append(template('tpl-shop', res));
+		$('#pnl-shop').append(template('tpl-shop', res));
+		$('#pnl-shop').append(template('tpl-shop', res));
+		$('#pnl-shop').append(template('tpl-shop', res));
+		$('#pnl-shop').append(template('tpl-shop', res));
+		$('#pnl-shop').append(template('tpl-shop', res));
+		$('#pnl-shop').append(template('tpl-shop', res));
+		$('#pnl-shop').append(template('tpl-shop', res));
+		$('#pnl-shop').append(template('tpl-shop', res));
+		$('#pnl-shop').append(template('tpl-shop', res));
+		$('#pnl-shop').append(template('tpl-shop', res));
+		*/
+		
+		mui('#refreshContainer').pullRefresh().endPullupToRefresh();//参数为true代表没有更多数据了。
+	});
+}
+
+mui.init({
+	pullRefresh   : {
+		container : "#refreshContainer",//下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
+		up        : {
+			//contentdown    : "下拉可以刷新",//可选，在下拉可刷新状态时，下拉刷新控件上显示的标题内容
+			//contentover    : "释放立即刷新",//可选，在释放可刷新状态时，下拉刷新控件上显示的标题内容
+			contentrefresh : "正在刷新...",//可选，正在刷新状态时，下拉刷新控件上显示的标题内容
+			callback       : funcPullupRefresh //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
+		}
+	}
+});
+
+function reinit (data) {
+	data = data || {};
+	_Data.page    = 0;
+	_Data.cate_id = data.category || '';
+	
+	$('#pnl-shop').empty();
+	mui('#refreshContainer').pullRefresh().pullupLoading();
+}
+
+mui.plusReady(function () {
+	plus.geolocation.getCurrentPosition(
+		function (res) {
+			_Data.lng = res.coords.longitude;
+			_Data.lat = res.coords.latitude;
+			
+			reinit ();
+		}, 
+		function () {
+			reinit ();
+		}, 
+		{ provider : 'baidu' }
+	);
+});
+
+template.helper('image', function (v) {
+	return v ? (app.link.image + v) : '../img/iconfont-morentouxiang.png';
+});
+
+
+
+	console.log('获取水站分类：' + app.url('mobile/cateseller/water_seller_cate'));
+	//plus.nativeUI.showWaiting('加载中...');
+	$.ajax({
+		'dataType' : 'json',
+		'type'     : 'post',
+		'url'      : app.url('mobile/cateseller/water_seller_cate'),
+		'data'     : mui.extend({}, _Data, {'page':_Data.page})
+	})
+	.fail(function (res) {
+		console.log('取得水站商家分类失败：' + JSON.stringify(res));
+		app.error('取得水站商家分类失败');
+		plus.nativeUI.closeWaiting();
+	})
+	.done(function (res) {
+		console.log('水站分类：' + JSON.stringify(res));
+		plus.nativeUI.closeWaiting();
+		
+		if (res.error && res.error.msg) { app.error(res.error.msg); return; }
+		if (false == res.status) {app.error(res.msg); return;};
+		if (res.msg) { plus.nativeUI.toast(res.msg); };
+		
+		$('#pnl-category').append(template('tpl-category', res));
+		$('[data-primary-category]:first').trigger('tap');
+	})
+	;
+	
+// 点击大分类
+$('body').delegate('[data-primary-category]', 'tap', function () {
+	var dom = this; id = $(this).attr('data-primary-category');
+	
+	$('[data-primary-category]').removeClass('active');
+	$(dom).addClass('active');
+	
+	$('[data-parent]').removeClass('active');
+	$('[data-parent='+id+']').addClass('active');
+	
+	if (!id) {$('[data-parent='+id+'] li').trigger('tap');}
+});
+// 点击小分类
+$('body').delegate('[data-secondary-category]', 'tap', function () {
+	var dom = this, id = $(dom).attr('data-secondary-category');
+	$('[data-secondary-category]').removeClass('active');
+	$(dom).addClass('active');
+	
+	
+	mui('.mui-slider').slider().gotoItem(0);
+	
+	setTimeout(function () { reinit({category:id}); }, 500);
+	
+	// 切换TAB页回本社区
+	//var js = '$(\'[data-page="station.shop.scroll.html"]\').addClass("activet").siblings().removeClass("activet");'
+	//plus.webview.getWebviewById('station.html').evalJS(js);
+	// 重新加载数据
+	//var view = plus.webview.getWebviewById('station.shop.scroll.html');
+	//view.show();
+	//view.evalJS('reinit({category:"'+(id||'')+'"})');
+});
+
+
+
+
